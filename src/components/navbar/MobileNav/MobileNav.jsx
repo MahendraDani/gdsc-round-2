61e0bb004f6f5405c268dd3f5a3d883d0ad3729f
Mahendra Dani <danimahendra0904@gmail.com>
2023-10-16 12:24:42 +0530

    feat: created layout of mobile navbar

---
+++ src/components/navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Close, Menu } from "@mui/icons-material";
+import { Box, IconButton, Stack } from "@mui/material";
+import NavLink from "../NavLink";
+
+const MobileNav = () => {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  const handleShowMobileNav = () => {
+    setShowMobileNav(true);
+  };
+  const handleCloseMovileNav = () => {
+    setShowMobileNav(false);
+  };
+  const NavLinks = [
+    {
+      name: "Vectors",
+      to: "/",
+    },
+    {
+      name: "Photos",
+      to: "/",
+    },
+    {
+      name: "PSD",
+      to: "/",
+    },
+    {
+      name: "Video",
+      to: "/",
+    },
+  ];
+
+  return (
+    <Box>
+      <Box onClick={handleShowMobileNav}>
+        <Menu />
+      </Box>
+      {showMobileNav && (
+        <Stack
+          id="mobile-nav"
+          sx={{
+            position: "fixed",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: "50%",
+            bgcolor: "white.main",
+            p: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Stack
+            direction={"column"}
+            sx={{ gap: 2, justifyContent: "center", alignItems: "start" }}
+          >
+            {NavLinks.map((link, index) => {
+              return <NavLink name={link.name} to={link.to} key={index} />;
+            })}
+          </Stack>
+          <Box
+            sx={{ position: "fixed", top: 16, right: 16, cursor: "pointer" }}
+            onClick={handleCloseMovileNav}
+          >
+            <Close />
+          </Box>
+        </Stack>
+      )}
+    </Box>
+  );
+};
+
+export default MobileNav;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,29 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 import NavLinkContainer from "./NavLinkContainer";
+import MobileNav from "./MobileNav/MobileNav";
 
 const Navbar = () => {
-  const NavLinks = [
-    {
-      name: "Vectors",
-      to: "/",
-    },
-    {
-      name: "Photos",
-      to: "/",
-    },
-    {
-      name: "PSD",
-      to: "/",
-    },
-    {
-      name: "Video",
-      to: "/",
-    },
-  ];
   return (
     <Box>
-      <Container sx={{ p: 3 }}>
-        <NavLinkContainer />
+      <Container sx={{ p: { md: 3, xs: 2 } }}>
+        <Stack
+          direction={"row"}
+          sx={{ justifyContent: "space-between", alignItems: "center" }}
+        >
+          <Stack
+            direction={"row"}
+            sx={{ justifyContent: "start", alignItems: "center", gap: 3 }}
+          >
+            <Typography>Logo</Typography>
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+              <NavLinkContainer />
+            </Box>
+          </Stack>
+          <Box sx={{ display: { md: "none" } }}>
+            <MobileNav />
+          </Box>
+        </Stack>
       </Container>
     </Box>
   );
